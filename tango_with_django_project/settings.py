d10883e705bd2ea6fb0a38a97dc1af9566b78dff
Yaqi <2515366z@student.gla.ac.uk>
2021-07-27 22:55:06 +0100

    4.2 & 4.3

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -55,7 +55,11 @@ ROOT_URLCONF = 'tango_with_django_project.urls'
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [STATIC_DIR, ]
+
+MEDIA_DIR = os.path.join(BASE_DIR, 'media')
+
+MEDIA_ROOT = MEDIA_DIR
+MEDIA_URL = '/media/'
 
 TEMPLATES = [
     {
@@ -68,6 +72,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media'
             ],
         },
     },
@@ -124,6 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [STATIC_DIR, ]
 
 print(__file__)
 print(os.path.dirname(__file__))
